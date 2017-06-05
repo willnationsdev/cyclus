6383e358b9af43213e8e8634578874b445606955
Will Nations <willnationsdev@gmail.com>
2017-06-05 17:05:06 -0500

    basic tiles for a grid. Partial 1
    Got a tileset scene setup with tiles and am painting them to a TileMap,
    but I can't seem to get my map_tool script working.

--- data/operation.gd
+++ data/operation.gd
@@ -1,13 +1,17 @@
 extends Node
 
 # Operations tell us...
-# 1. Who
+# 1. Who (player, character, and team)
 # 2. did what
 # 3. where
 # 4. when (turn, step, and task)
 
-# Which player executed the operation
+# Which player executed the operation (multiplayer network unique ID)
 onready var player_id = 1
+# Which character the player is using, if applicable
+onready var character_id = 1
+# Which team the player is on, if applicable
+onready var team_id = 1
 # Which operation the player executed
 onready var op_name = ""
 # Where the player executed it
