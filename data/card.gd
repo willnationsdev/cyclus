50d4b2ecf25a84d13cdacfe524254ff558cfcd65
Will Nations <willnationsdev@gmail.com>
2017-06-08 17:01:18 -0500

    stuff

--- data/card.gd
+++ data/card.gd
@@ -1,7 +1,12 @@
 # Card data
 
-
 extends Node
+
+var data = {
+	name = "",
+	cost = 0,
+	effects = []
+}
 
 func _ready():
 	pass
---
+++ ui/player_deck_info/player_deck_info.gd
@@ -0,0 +1,7 @@
+extends Control
+
+onready var cards = $menu_background/player_container/deck_data/deck_cards
+onready var stats = $menu_background/player_container/deck_data/deck_stats
+
+func _ready():
+	pass
